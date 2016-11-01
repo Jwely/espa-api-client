f481dabec9c7d78ea502ee15fd21b72c08c0ccc4
Jeff Ely <jeff.ely.08@gmail.com>
2016-11-01 17:29:23 -0400

    fixed setup to build for python 2.7+

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # espa-api-client
-`current-version = 1.0.0.dev4`
+`current-version = 1.0.0.dev5`
 
 Python interface to the [API for ordering from ESPA](https://github.com/USGS-EROS/espa-api). This API allows lost of custom processing of surface reflectance corrected data for landsat, and some other very useful science grade computations. So, this python client provides some friendly interfaces with the API to make life a little easier.
 
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0.dev4',
+    version='1.0.0.dev5',
 
     description='Python interface to the espa-api',
     long_description=long_description,
@@ -54,6 +54,9 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
