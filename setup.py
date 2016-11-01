d774a15b23e54d1c69ce01f6b406dcb630e3434b
Jeff Ely <jeff.ely.08@gmail.com>
2016-11-01 17:27:03 -0400

    no longer uses pandas

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0.dev3',
+    version='1.0.0.dev4',
 
     description='Python interface to the espa-api',
     long_description=long_description,
@@ -80,8 +80,8 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['pandas'],
-        'test': ['pandas'],
+        'dev': [],
+        'test': [],
     },
 
     # If there are data files included in your packages that need to be
