12ab099a811539e695dfd3f67b86d3f5bcfd2d28
Jeff Ely <jeff.ely.08@gmail.com>
2016-12-14 13:38:44 -0500

    fixed parser, EE dumps csvs which are in Latin encoding instead of UTF-8

--- espa_api_client/parse.py
+++ espa_api_client/parse.py
@@ -1,15 +1,11 @@
+import pandas as pd
 
 
-# now without using pandas!
 def get_order_inputs_from_earth_explorer_export(csv_path):
     """
     All the landsat record exports come with the tilename in the first column, so
     this simple function just reads it and returns that whole first column as a list.
     """
-    tiles = []
-    with open(csv_path, 'r') as f:
-        for line in f.readlines():
-            first_col = line.split(',')[0]
-            if " " not in first_col:    # ignore headers by throwing out spaces
-                tiles.append(first_col)
+    df = pd.read_csv(csv_path, encoding="ISO-8859-1")
+    tiles = df['Landsat Scene Identifier']
     return tiles
