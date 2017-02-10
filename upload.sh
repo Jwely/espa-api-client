b0049c503d7003374c55485c2a71e2ad7dfb20b6
Jeff Ely <jeff.ely.08@gmail.com>
2017-02-10 13:47:03 -0500

    venv seems to block removal of temp dirs, added sudo?

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sudo apt-get install twine
 twine upload dist/*
-rm -rf build
-rm -rf dist
-rm -rf espa_api_client.egg-info
+sudo rm -rf build
+sudo rm -rf dist
+sudo rm -rf espa_api_client.egg-info
 sudo pip3 install --upgrade espa-api-client
 sudo pip install --upgrade espa-api-client
